dfbd1addf18bc46e49acd8ff319ec6359af1c153
Umair Ahmed <57555747+umairTech981@users.noreply.github.com>
2023-12-06 22:31:38 +0530

    Create Triangle Or Circle .cpp

---
+++ Triangle Or Circle .cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    double h,b;
+    cout<<"Enter Height : ";
+    cin>>h;
+    cout<<"Enter Base : ";
+    cin>>b;
+    cout<<"Area Of Triangle = ";
+    cout<<(1.0/2.0*b*h)<<endl;
+    
+    double r;
+    cout<<"Enter Radius : ";
+    cin>>r;
+    cout<<"Area Of Circle = ";
+    cout<<(3.14*r*r);
+    
+    return 0;
+}
